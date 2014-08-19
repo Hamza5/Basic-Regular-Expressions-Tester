ded3bea9bf7eacd3cb8455efcf8c3c10fb61b51d
Hamza5 <hamza.abbad@gmail.com>
2014-08-19 16:41:16 +0100

    Search and replace is working

--- BasicRegularExpressionsTester.py
+++ BasicRegularExpressionsTester.py
@@ -18,8 +18,15 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 
+__appName__ = 'BRET'
+__fullAppName__ = 'Basic Regular Expressions Tester'
+__version__ = '0.2'
+
 import sys
 import re
+from urllib.request import urlopen, Request
+from urllib.error import URLError
+
 try :
 	from PyQt4.QtGui import QApplication, QWidget, QFileDialog, QMessageBox, QTextEdit, QStandardItem, QStandardItemModel
 	from PyQt4.QtCore import QObject, SIGNAL, Qt, QUrl
@@ -33,6 +40,10 @@ try :
 except ImportError as e :
 	QMessageBox.critical(None, "FATAL ERROR", "<h3>"+e.msg.capitalize()+"</h3>" + "<p>"+_translate("Window", "Your copy of BRET may be broken", None)+"</p>")
 	sys.exit(1)
+
+class InvalidURLError(URLError):
+	def __init__(self, msg):
+		super().__init__(self, msg)
 
 class MainWindow(QWidget, Ui_Window):
 	def __init__(self, parent=None):
@@ -58,7 +69,7 @@ class MainWindow(QWidget, Ui_Window):
 		QObject.connect(self.FilePathLineEdit, SIGNAL("textChanged(const QString&)"), self.lineEditChange) # Same for the file path edit.
 		QObject.connect(self.URLLineEdit, SIGNAL("textChanged(const QString&)"), self.lineEditChange) # Same for the URL edit.
 		QObject.connect(self.MethodTabWidget, SIGNAL("currentChanged(int)"), self.tabChange) # Same for the file path edit.
-		QObject.connect(self.FindMatchesPushButton, SIGNAL("clicked()"), self.searchAndReplace)
+		QObject.connect(self.FindMatchesPushButton, SIGNAL("clicked()"), self.searchAndReplace) # 'Search and replace button'
 		QObject.connect(self.ResetMatchesPushButton, SIGNAL("clicked()"), self.matchesModel.clear) # Clear the results of the match.
 		QObject.connect(self.clipboard, SIGNAL("dataChanged()"), self.clipboardDataChanged) # Enable/disable the 'Paste from clipboard' buttons.
 
@@ -119,6 +130,7 @@ class MainWindow(QWidget, Ui_Window):
 			regexp = re.compile(self.RegExpLineEdit.text(), flags = ignoreCase | multiline | pointAll | ASCIIOnly)
 		except re.error as e: # Syntax errors in the RegExp.
 			QMessageBox.warning(self, _translate("Window", "Invalid regular expression", None), "<h3>"+_translate("Window", "The regular expression you entred is invalid !", None)+"</h3><p>"+_translate("Window", "Cause : "+str(e), None)+"</p>")
+			self.matchesModel.clear()
 			self.RegExpLineEdit.selectAll() # Select all the text in the RegExp line edit.
 		return regexp # The valid RegExp or None.
 	
@@ -126,38 +138,79 @@ class MainWindow(QWidget, Ui_Window):
 		text = ''
 		textFile = None
 		try :
-			textFile = open(filePath, 'r', encoding="utf-8")
+			textFile = open(self.FilePathLineEdit.text(), 'r', encoding="utf-8")
 			text =  textFile.read()
-			return text # File content.
+			textFile.close()
 		except FileNotFoundError :
 			# Show the error message dialog.
-			QMessageBox.warning(self, _translate("Window", "File not found", None), "<h3>"+_translate("Window", "The selected file can not be found !", None)+"</h3><p>"+_translate("Window", "Please verify that the file exist", None)+"</p>")
+			QMessageBox.warning(self, _translate("Window", "File not found", None), "<h3>"+_translate("Window", "The selected file can not be found !", None)+"</h3><p>"+_translate("Window", "Verify that the file exist", None)+"</p>")
 		except PermissionError :
 			QMessageBox.warning(self, _translate("Window", "Permission error", None), "<h3>"+_translate("Window", "You don't have the required permissions to open this file !", None)+"</h3><p>"+_translate("Window", "It seems that the owner of this file didn't give you the permission of reading", None)+"</p>")
 		except UnicodeError :
-			QMessageBox.warning(self, _translate("Window", "Invalid file", None), "<h3>"+_translate("Window", "The file is not a valid Unicode text file !", None)+"</h3><p>"+_translate("Window", "Maybe because the selected file is not a text file or it is corrupted", None)+"</p>")
+			QMessageBox.warning(self, _translate("Window", "Invalid file", None), "<h3>"+_translate("Window", "The file doesn't contain valid Unicode text !", None)+"</h3><p>"+_translate("Window", "Maybe because the selected file is not a text file or it is corrupted", None)+"</p>")
 		except OSError as e :
-			QMessageBox.warning(self, _translate("Window", "Failed to use the selected file", None), "<h3>"+_translate("Window", "Can not open the selected file", None)+"</h3><p>"+e.strerror+"</p>")
+			QMessageBox.warning(self, _translate("Window", "Failed to use the selected file", None), "<h3>"+_translate("Window", "Can not open the selected file !", None)+"</h3><p>"+e.strerror+"</p>")
 		#except Exception :
 			#QMessageBox.warning(self, _translate("Window", "Unknown error", None), "<h3>"+_translate("Window", "An error occurred", None)+"</h3><p>"+str(e)+"</p>")
 		finally :
-			if textFile : textFile.close() # Close the file if no error occurred.
+			return text # File content or empty.
+
 	def loadURL(self): # Try to download and read the file :
-		pass
+		url = self.URLLineEdit.text()
+		content = ""
+		try :
+			if not QUrl(url).isValid() :
+				raise InvalidURLError("The URL you entred is invalid")
+			else :
+				request = Request(url, headers={'User-Agent':__appName__+'/'+__version__})
+				response = urlopen(request)
+				content = response.read() # The content of the URL.
+				response.close()
+		except UnicodeError :
+			QMessageBox.warning(self, _translate("Window", "Invalid downloaded file", None), "<h3>"+_translate("Window", "The file downloaded doesn't contain valid Unicode text !", None)+"</h3><p>"+_translate("Window", "Maybe because the URL doesn't point to a text file or it is corrupted", None)+"</p>")
+		except InvalidURLError :
+			QMessageBox.warning(self, _translate("Window", "Invalid URL", None), "<h3>"+_translate("Window", "The URL you entred is invalid !", None)+"</h3><p>"+_translate("Window", "Check the URL syntax", None)+"</p>")
+		except URLError as e :
+			QMessageBox.warning(self, _translate("Window", "URL error", None), "<h3>"+_translate("Window", "Error occurred when using the URL !", None)+"</h3><p>"+str(e.reason)+"</p>")
+		finally :
+			return content
 
 	def searchAndReplace(self):
-		pass
-		#regexp = self.verifiedRegExp() 
-		#if not regexp : return
-		#self.matchesModel.clear()
-		#matches = bret.search(regexp, self.TextEdit.document().toPlainText(), 0 if self.NoMatLimitCheckBox.isChecked() else self.MatchesLimitSpinBox.value(), self.StartPositionSpinBox.value(), self.EndPositionSpinBox.value()) 
-		#self.matchesModel.setHorizontalHeaderLabels(['Match','From position','to position'])
-		#for m in matches :
-			#item = QStandardItem(m.group())
-			#self.matchesModel.appendRow([item, QStandardItem(str(m.start())), QStandardItem(str(m.end()))])
-			#if self.GroupsCheckBox.isChecked() :
-				#for i in range(len(m.groups())) :
-					#item.appendRow([QStandardItem(m.group(i+1)), QStandardItem(str(m.start(i+1))), QStandardItem(str(m.end(i+1)))])
+		regexp = self.verifiedRegExp() 
+		if not regexp : return
+		self.matchesModel.clear()
+		index = self.MethodTabWidget.currentIndex()
+		matches = None
+		content = ""
+		if index == 0 :
+			content = self.TextEdit.document().toPlainText()
+		elif index == 1 :
+			content = self.loadTextFile()
+		else :
+			content = self.loadURL()
+		if not content : return
+		end = len(content)-1
+		matches = bret.search(regexp, content, 0 if self.NoMatchesLimitCheckBox.isChecked() else self.MatchesLimitSpinBox.value(), 0, end) 
+		self.matchesModel.setHorizontalHeaderLabels(['Match','From position','to position'])
+		for m in matches :
+			item = QStandardItem(m.group())
+			item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+			startPositionItem = QStandardItem(str(m.start()))
+			startPositionItem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+			endPositionItem = QStandardItem(str(m.end()))
+			endPositionItem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+			self.matchesModel.appendRow([item, startPositionItem, endPositionItem])
+			if self.GroupsCheckBox.isChecked() :
+				for i in range(len(m.groups())) :
+					subgroupItem = QStandardItem(m.group(i+1))
+					subgroupItem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+					subgroupStartItem = QStandardItem(str(m.start(i+1)))
+					subgroupStartItem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+					subgroupEndItem = QStandardItem(str(m.end(i+1)))
+					subgroupEndItem.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+					item.appendRow([subgroupItem, subgroupStartItem, subgroupEndItem])
+				self.MatchesTreeView.expandAll()
+		self.MatchesTreeView.resizeColumnToContents(0)
 
 window = MainWindow()
 window.show()
